a4d8dc9695a1522448e73313330d105031a2dd49
Alexey Kolosha <akolosha@simtechdev.com>
2015-12-08 22:18:09 +0300

    init

---
+++ migrations/20151205224528_turn_off_ya_market_addons.php
@@ -0,0 +1,20 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+use Tygh\Registry; if (!defined('AREA')){define('AREA','A');} require_once dirname(dirname(__FILE__)).'/init.php';
+class TurnOffYaMarketAddons extends AbstractMigration
+{
+    public function up()
+    {
+        fn_uninstall_addon('yandex_market');
+        fn_uninstall_addon('yandex_search');
+        die();
+
+    }
+
+    public function down()
+    {
+
+
+    }
+}
